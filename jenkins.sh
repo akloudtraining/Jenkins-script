64f0e68b05e64bd4f5e79bd2b586ca6e313743de
Armand <akloudservice@gmail.com>
2023-06-11 11:57:52 -0400

    first commit

---
+++ jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This is cript is to install jenkins
+#date
+
+echo "Updating the system"
+sleep 4
+sudo apt update
+echo "Installing JAVA"
+sleep 4
+sudo apt install openjdk-11-jre -y
+java -version
+echo "Downloading the Jenkins Libraries"
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+echo "Installing Jenkins"
+sudo apt-get install jenkins -y
+sudo systemctl restart jenkins
+sleep 10
+sudo systemctl status jenkins
